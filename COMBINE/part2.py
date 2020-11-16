b36db843cd45316f67f412f9ae571445dcc9a728
Sawoud <alloss@mcmaster.ca>
2020-11-16 16:53:14 -0500

    part 2 changes

--- COMBINE/part2.py
+++ COMBINE/part2.py
@@ -10,6 +10,8 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.popup import Popup
 import os.path
 import serial
+import re
+import threading
 from kivy.uix.slider import Slider
 
 
@@ -25,7 +27,7 @@ def Connect(): # function that deermines if the pace maker is connected or not
     ser = serial
     com = "COM4"
     try:
-      ser = serial.Serial(com, 9600, timeout=1) # the fn. waits for 1 second, if it cant find a connection it determines its not connected
+      ser = serial.Serial(com, 9600, timeout=.025) # the fn. waits for 1 second, if it cant find a connection it determines its not connected
 
       while ser.read():
         connection = 0
@@ -90,9 +92,8 @@ class MyGrid(GridLayout):
 
 
     def Popup(self,var,temp): # this function shows the values of the parameters of each mode
-        str = user + "\\" + var +'.txt'
-        print(str)
-        f = open(str,"r")
+        strg = user + "\\" + var +'.txt'
+        f = open(strg,"r")
         popup = Popup(title=var+" Parameters",
         content=Label(text=f.read()),
         size_hint=(None, None), size=(400, 400))
@@ -101,6 +102,7 @@ class MyGrid(GridLayout):
 
     def para(self,var,select):
         global font_size
+        global t1
         self.clear_widgets()
         self.AddButtons(select)
         self.cols = 3
@@ -108,14 +110,14 @@ class MyGrid(GridLayout):
         self.name1 = TextInput(multiline=False) # ...and gives the user the chance to change them
         self.add_widget(self.name1)
         self.submit1 = Button(text = "Change",font_size = font_size)
-        self.submit1.bind(on_press = partial(self.pressed,self.name1,var,'Lower Rate Limit'))
+        self.submit1.bind(on_press = partial(self.pressed,self.name1,var,select,'Lower Rate Limit'))
         self.add_widget(self.submit1)
 
         self.add_widget(Label(text="Upper Rate Limit"))
         self.name2 = TextInput(multiline=False)
         self.add_widget(self.name2)
         self.submit2 = Button(text = "Change",font_size = font_size)
-        self.submit2.bind(on_press = partial(self.pressed,self.name2,var,'Upper Rate Limit'))
+        self.submit2.bind(on_press = partial(self.pressed,self.name2,var,select,'Upper Rate Limit'))
         self.add_widget(self.submit2)
 
         if(select == 6 or select == 7 or select == 8 or select == 9 or select == 10):
@@ -123,7 +125,7 @@ class MyGrid(GridLayout):
             self.name16 = TextInput(multiline=False)
             self.add_widget(self.name16)
             self.submit16 = Button(text = "Change",font_size = font_size)
-            self.submit16.bind(on_press = partial(self.pressed,self.name16,var,'Maximum Sensor Rate'))
+            self.submit16.bind(on_press = partial(self.pressed,self.name16,var,select,'Maximum Sensor Rate'))
             self.add_widget(self.submit16)
 
 
@@ -132,7 +134,7 @@ class MyGrid(GridLayout):
             self.name14 = TextInput(multiline=False)
             self.add_widget(self.name14)
             self.submit14 = Button(text = "Change",font_size = font_size)
-            self.submit14.bind(on_press = partial(self.pressed,self.name14,var,'Fixed AV Delay'))
+            self.submit14.bind(on_press = partial(self.pressed,self.name14,var,select,'Fixed AV Delay'))
             self.add_widget(self.submit14)
 
         if(select == None):
@@ -140,7 +142,7 @@ class MyGrid(GridLayout):
             self.name15 = TextInput(multiline=False)
             self.add_widget(self.name15)
             self.submit15 = Button(text = "Change",font_size = font_size)
-            self.submit15.bind(on_press = partial(self.pressed,self.name15,var,'Dynamic AV Delay'))
+            self.submit15.bind(on_press = partial(self.pressed,self.name15,var,select,'Dynamic AV Delay'))
             self.add_widget(self.submit15)
 
 
@@ -149,14 +151,14 @@ class MyGrid(GridLayout):
             self.name3 = TextInput(multiline=False)
             self.add_widget(self.name3)
             self.submit3 = Button(text = "Change",font_size = font_size)
-            self.submit3.bind(on_press = partial(self.pressed,self.name3,var,'Atrial Amplitude'))
+            self.submit3.bind(on_press = partial(self.pressed,self.name3,var,select,'Atrial Amplitude'))
             self.add_widget(self.submit3)
         if(select == 1 or select == 3  or select == 5 or select == 6  or select == 8 or select == 10):
             self.add_widget(Label(text="Atrial Pulse Width"))
             self.name4 = TextInput(multiline=False)
             self.add_widget(self.name4)
             self.submit4 = Button(text = "Change",font_size = font_size)
-            self.submit4.bind(on_press = partial(self.pressed,self.name4,var,'Atrial Pulse Width'))
+            self.submit4.bind(on_press = partial(self.pressed,self.name4,var,select,'Atrial Pulse Width'))
             self.add_widget(self.submit4)
 
         if(select == 2 or select == 4  or select == 5 or select == 7  or select == 9 or select == 10):
@@ -164,7 +166,7 @@ class MyGrid(GridLayout):
             self.name5 = TextInput(multiline=False)
             self.add_widget(self.name5)
             self.submit5 = Button(text = "Change",font_size = font_size)
-            self.submit5.bind(on_press = partial(self.pressed,self.name5,var,'Ventricular Amplitude'))
+            self.submit5.bind(on_press = partial(self.pressed,self.name5,var,select,'Ventricular Amplitude'))
             self.add_widget(self.submit5)
 
         if(select == 2 or select == 4  or select == 5 or select == 7 or select == 9 or select == 10):
@@ -172,7 +174,7 @@ class MyGrid(GridLayout):
             self.name6 = TextInput(multiline=False)
             self.add_widget(self.name6)
             self.submit6 = Button(text = "Change",font_size = font_size)
-            self.submit6.bind(on_press = partial(self.pressed,self.name6,var,'Ventricular Pulse Width'))
+            self.submit6.bind(on_press = partial(self.pressed,self.name6,var,select,'Ventricular Pulse Width'))
             self.add_widget(self.submit6)
 
 
@@ -181,7 +183,7 @@ class MyGrid(GridLayout):
             self.name8 = TextInput(multiline=False)
             self.add_widget(self.name8)
             self.submit8 = Button(text = "Change",font_size = font_size)
-            self.submit8.bind(on_press = partial(self.pressed,self.name8,var,'Atrial Sensitivity'))
+            self.submit8.bind(on_press = partial(self.pressed,self.name8,var,select,'Atrial Sensitivity'))
             self.add_widget(self.submit8)
 
         if(select == 9):
@@ -189,7 +191,7 @@ class MyGrid(GridLayout):
             self.name21 = TextInput(multiline=False)
             self.add_widget(self.name21)
             self.submit21 = Button(text = "Change",font_size = font_size)
-            self.submit21.bind(on_press = partial(self.pressed,self.name21,var,'VRP'))
+            self.submit21.bind(on_press = partial(self.pressed,self.name21,var,select,'VRP'))
             self.add_widget(self.submit21)
 
         if(select == 3  or select == 8 or select == 9):
@@ -197,7 +199,7 @@ class MyGrid(GridLayout):
             self.name9 = TextInput(multiline=False)
             self.add_widget(self.name9)
             self.submit9 = Button(text = "Change",font_size = font_size)
-            self.submit9.bind(on_press = partial(self.pressed,self.name9,var,'ARP'))
+            self.submit9.bind(on_press = partial(self.pressed,self.name9,var,select,'ARP'))
             self.add_widget(self.submit9)
 
         if(select == 3  or select == 8):
@@ -205,7 +207,7 @@ class MyGrid(GridLayout):
             self.name10 = TextInput(multiline=False)
             self.add_widget(self.name10)
             self.submit10 = Button(text = "Change",font_size = font_size)
-            self.submit10.bind(on_press = partial(self.pressed,self.name10,var,'PVARP'))
+            self.submit10.bind(on_press = partial(self.pressed,self.name10,var,select,'PVARP'))
             self.add_widget(self.submit10)
 
         if(select == 3 or select == 4 or select == 8  or select == 9):
@@ -213,7 +215,7 @@ class MyGrid(GridLayout):
             self.name11 = TextInput(multiline=False)
             self.add_widget(self.name11)
             self.submit11 = Button(text = "Change",font_size = font_size)
-            self.submit11.bind(on_press = partial(self.pressed,self.name11,var,'Hysteresis'))
+            self.submit11.bind(on_press = partial(self.pressed,self.name11,var,select,'Hysteresis'))
             self.add_widget(self.submit11)
 
         if(select == 3 or select == 4  or select == 8  or select == 9):
@@ -221,7 +223,7 @@ class MyGrid(GridLayout):
             self.name12 = TextInput(multiline=False)
             self.add_widget(self.name12)
             self.submit12 = Button(text = "Change",font_size = font_size)
-            self.submit12.bind(on_press = partial(self.pressed,self.name12,var,'Rate Smoothing'))
+            self.submit12.bind(on_press = partial(self.pressed,self.name12,var,select,'Rate Smoothing'))
             self.add_widget(self.submit12)
 
         if(select == 4 or select == 9):
@@ -229,7 +231,7 @@ class MyGrid(GridLayout):
             self.name13 = TextInput(multiline=False)
             self.add_widget(self.name13)
             self.submit13 = Button(text = "Change",font_size = font_size)
-            self.submit13.bind(on_press = partial(self.pressed,self.name13,var,'Ventricular Sensitivity'))
+            self.submit13.bind(on_press = partial(self.pressed,self.name13,var,select,'Ventricular Sensitivity'))
             self.add_widget(self.submit13)
 
 
@@ -238,7 +240,7 @@ class MyGrid(GridLayout):
             self.name17 = TextInput(multiline=False)
             self.add_widget(self.name17)
             self.submit17 = Button(text = "Change",font_size = font_size)
-            self.submit17.bind(on_press = partial(self.pressed,self.name17,var,'Activity Threshold'))
+            self.submit17.bind(on_press = partial(self.pressed,self.name17,var,select,'Activity Threshold'))
             self.add_widget(self.submit17)
 
         if(select == 6 or select == 7 or select == 8 or select == 9 or select == 10):
@@ -246,7 +248,7 @@ class MyGrid(GridLayout):
             self.name18 = TextInput(multiline=False)
             self.add_widget(self.name18)
             self.submit18 = Button(text = "Change",font_size = font_size)
-            self.submit18.bind(on_press = partial(self.pressed,self.name18,var,'Reaction Time'))
+            self.submit18.bind(on_press = partial(self.pressed,self.name18,var,select,'Reaction Time'))
             self.add_widget(self.submit18)
 
         if(select == 6 or select == 7 or select == 8 or select == 9 or select == 10):
@@ -254,7 +256,7 @@ class MyGrid(GridLayout):
             self.name19 = TextInput(multiline=False)
             self.add_widget(self.name19)
             self.submit19 = Button(text = "Change",font_size = font_size)
-            self.submit19.bind(on_press = partial(self.pressed,self.name19,var,'Response Factor'))
+            self.submit19.bind(on_press = partial(self.pressed,self.name19,var,select,'Response Factor'))
             self.add_widget(self.submit19)
 
 
@@ -263,7 +265,7 @@ class MyGrid(GridLayout):
             self.name20 = TextInput(multiline=False)
             self.add_widget(self.name20)
             self.submit20 = Button(text = "Change",font_size = font_size)
-            self.submit20.bind(on_press = partial(self.pressed,self.name20,var,'Recovery Time'))
+            self.submit20.bind(on_press = partial(self.pressed,self.name20,var,select,'Recovery Time'))
             self.add_widget(self.submit20)
 
 
@@ -294,6 +296,7 @@ class MyGrid(GridLayout):
     def AOO(self,temp): # This is the AOO function
         var = "AOO"
         self.para(var,1)
+
 ####################################################
 
     def VOO(self,temp): # This is the VOO function
@@ -339,51 +342,61 @@ class MyGrid(GridLayout):
         self.para(var,10)
 ####################################################
 
-    def pressed(self,instance,mode,var,temp3): # this function is invoked when the user presses any change button
-        global user
 
-        str = user + '\\' + mode+'.txt' # the path for the required file
-        print(str)
-        f = open(str, "r+")
+
+
+    def pressed(self,instance,mode,select,var,temp3): # this function is invoked when the user presses any change button
+        global user
+        strg = user + '\\' + mode+'.txt' # the path for the required file
+        f = open(strg, "r+")
         lines = f.readlines()
-        print(lines)
         i = 0
         line=""
         for line in lines: # this for loop finds where the requested parameter is located
 
             pos = line.find("(")
-            print(pos)
             pos1 = line.find("\n")
-            print(pos1)
             line = line[:pos] + line[pos1+1:]
-            print("line",line)
-            print("var",var)
+
             if(line == var):
                 break
             else:
                 i = i + 1
-        print(line)
         line = line + "(" + instance.text + ")" + "\n" # the line gets the parameter inputed between the brackets
-        print(line)
-        print("1",lines)
 
         lines[i] = line
-        print("2",lines)
         lines = ''.join(lines)
-        print("3",lines)
         f.seek(0)
         f.write(lines)# file gets overwritten with the new information
         f.truncate()
         f.close()
+        lines  =  ((re.findall('\(.*?\)',lines)))
+        for i in range(len(lines)):
+            temp = list(lines[i])
+            temp.pop(0)
+            temp.pop(-1)
+            temp = "".join(temp)
+            lines[i] = temp
+        lines.insert(0,str(select))
+        for line in lines:
+             sendData(line)
 
 class MyApp(App):
     def build(self):
         return MyGrid()
 
 user = ""
+
+def sendData(data):
+    path = serial.Serial('COM4', 115200)
+    print(data)
+    path.write(data.encode())
+    print(data.encode())
+
 def main(info):
     global user
     user = info
+
     print("this is",info)
     MyApp().run()
 
